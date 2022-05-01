92bb2a8aa2f4eee6c6061f3c372a01507da9f9ae
Sejal Rai <sejalrai23@gmail.com>
2022-05-01 10:44:31 +0530

    first commit

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
-from PIL import Image
+# from PIL import Image
 import tensorflow as tf
 import requests
 
